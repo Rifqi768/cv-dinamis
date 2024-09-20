993c047586f8628fc61e62f2193f672ac4b18ed2
Rifqi768 <148458885+Rifqi768@users.noreply.github.com>
2024-09-20 09:32:21 +0700

    initial comit

---
+++ resources/views/biodata/create.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>formulir</h1>
+        <form method="post" action="{{ route('biodata.store')}}">
+            @csrf
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">nama</label>
+                <input type="nama" class="form-control" id="" aria-describedby="" placeholder="Tambahkan nama" name="nama" required>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Alamat</label>
+                <input type="nama" class="form-control" id="" aria-describedby="" placeholder="Tambahkan Alamat" name="alamat" required>
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">keterangan</label>
+                 <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="keterangan" required></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+@endsection
